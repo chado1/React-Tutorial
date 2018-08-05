f2a1cf4b2cf5aae73561eb2e6be866ae0f2747ae
Chad Oberg <chad.oberg@calgary.ca>
2018-08-05 16:40:46 -0500

    video search enabled

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
-const API_KEY = ''
+const API_KEY = 'YOUR_API_KEY'
 
 class App extends Component {
     constructor(props) {
@@ -15,18 +16,24 @@ class App extends Component {
             selectedVideo: null 
         };
 
-        YTSearch({key: API_KEY, term: 'coleco adam'}, (videos) => {
-            this.setState({
-                videos: videos,
-                selectedVideo: videos[0]
-             });
-        });
+        this.videoSearch('coleco adam');
     }
 
+videoSearch(term){
+    YTSearch({key: API_KEY, term: term}, (videos) => {
+        this.setState({
+            videos: videos,
+            selectedVideo: videos[0]
+         });
+    });
+}
+
     render() {
+        const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 500);
+
         return (
             <div>
-                <SearchBar />
+                <SearchBar onSearchTermChange={videoSearch}/>
                 <VideoDetail video={this.state.selectedVideo} />
                 <VideoList
                     onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
